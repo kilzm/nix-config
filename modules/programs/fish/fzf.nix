2858d5eb7a02e28a128a8d7f31d37eaec13ef947
Kilian Markl <kilian02.markl@gmail.com>
2026-05-20 17:23:26 +0200

    fish: use fzf.fish

--- modules/programs/fish/fzf.nix
+++ modules/programs/fish/fzf.nix
@@ -8,17 +8,20 @@
       fzf
     ];
 
-    configFile.content = "${lib.getExe pkgs.fzf} --fish | source";
+    plugins = with pkgs.fishPlugins; [
+      fzf-fish
+    ];
 
     env = {
       FZF_DEFAULT_OPTS = lib.concatStringsSep " " [
         "--ansi"
         "--color=16"
         "--layout reverse"
-        "--height 70%"
-        "--margin 4%"
-        "--border rounded"
+        "--height 90%"
+        "--margin 1"
+        "--border none"
       ];
+      LS_COLORS =  "di=34:ln=36:ex=32";
     };
   };
 }
