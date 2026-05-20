9a027f928ac33624a394c68733c3246570012c42
Kilian Markl <kilian02.markl@gmail.com>
2026-05-20 17:22:51 +0200

    env: refactor

--- modules/programs/env.nix
+++ modules/programs/env.nix
@@ -7,57 +7,49 @@
   }: {
     packages.cli-env = pkgs.symlinkJoin {
       name = "cli-env";
-      paths =
-        (with pkgs; [
-          zoxide
-          htop
-          killall
-          pciutils
-          usbutils
-          evtest
-          netcat
-          wget
-          _7zz
-          ripgrep
-          imagemagick
-          libqalculate
-          fd
-          jq
-          dust
-          trashy
-          gdb
-          gef
-          lldb
-          python3
-        ])
-        ++ (with self'.packages; [
-          bat
-        ]);
+      paths = with pkgs; [
+        htop
+        killall
+        pciutils
+        usbutils
+        evtest
+        netcat
+        wget
+        _7zz
+        ripgrep
+        imagemagick
+        libqalculate
+        fd
+        jq
+        dust
+        trashy
+        gdb
+        gef
+        lldb
+        gcc
+        python3
+      ];
     };
 
     packages.env = inputs.wrapper-modules.lib.wrapPackage {
       inherit pkgs;
       package = self'.packages.fish;
-      prefixVar = [
-        [
-          "PATH"
-          ":"
-          (lib.makeBinPath (
-            (with self'.packages; [
-              cli-env
-              neovim
-              tmux
-              git
-            ])
-            ++ (
-              with pkgs; [
-                direnv
-                fzf
-                lazygit
-              ]
-            )
-          ))
-        ]
+      prefixVar = let
+        packages =
+          (with self'.packages; [
+            cli-env
+            neovim
+            tmux
+            git
+          ])
+          ++ (with pkgs; [
+            direnv
+            lazygit
+            zoxide
+            bat
+          ]);
+      in [
+        ["PATH" ":" lib.makeBinPath packages]
       ];
     };
   };
