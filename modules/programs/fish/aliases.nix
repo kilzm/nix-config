da8aac553fcd84cfe2ec4313528dd67acc2830ce
Kilian Markl <kilian02.markl@gmail.com>
2026-05-22 18:52:38 +0200

    fish: update abbreviations

--- modules/programs/fish/aliases.nix
+++ modules/programs/fish/aliases.nix
@@ -13,71 +13,116 @@
           lla = "-la";
           lt = "--tree";
         }
-        |> lib.mapAttrs (_: v: "${lib.getExe self'.packages.eza} ${v}");
+        |> lib.mapAttrs (
+          _: v: "${lib.getExe self'.packages.eza} ${v}"
+        );
     in
-      {}
-      // ezaAliases;
+      ezaAliases;
 
     abbreviations = let
       nixAbbreviations = let
-        simple = {
-          nd = "nix develop";
-          ns = "nix shell";
-          nb = "nix build";
-          nr = "nix run";
-        };
-
-        local =
-          simple
-          |> lib.mapAttrs' (n: v:
-            lib.nameValuePair "${n}." {
-              cursor = true;
-              expansion = "${v} .#%";
-            });
-
-        nixpkgs =
-          simple
-          |> lib.mapAttrs' (n: v:
-            lib.nameValuePair "${n}n" {
-              cursor = true;
-              expansion = "${v} nixpkgs#%";
-            });
+        mkAbbr = suffix: mkEntry:
+          {
+            d = "develop";
+            s = "shell";
+            b = "build";
+            r = "run";
+          }
+          |> lib.mapAttrs' (
+            n: v:
+              lib.nameValuePair
+              "n${n}${suffix}"
+              (mkEntry "nix ${v}" // {position = "command";})
+          );
       in
-        simple // local // nixpkgs;
+        mkAbbr "" (v: {
+          expansion = v;
+        })
+        // mkAbbr "." (v: {
+          cursor = true;
+          expansion = "${v} .#%";
+        })
+        // mkAbbr "n" (v: {
+          cursor = true;
+          expansion = "${v} nixpkgs#%";
+        });
 
-      gitAbbreviations = {
-        gd = "git diff";
-        ga = "git add";
-        gc = "git commit";
-        gp = "git push";
-        gu = "git pull";
-        gr = "git rebase";
-        gf = "git fetch";
-        gco = "git checkout";
-        gcb = "git checkout -b";
-        gm = "git merge";
-        gs = "git status --short";
-        gi = "git init";
-        gcl = "git clone";
-        gst = "git stash";
-        gsta = "git stash apply";
-        gstp = "git stash pop";
-        gstd = "git stash drop";
-        gsts = "git stash show -p";
-        gl = "git log --all --graph --pretty=format:\\\"%C(magenta)%h %C(white) %an %ar%C(auto) %D%n%s%n\\\"";
-      };
+      gitAbbreviations = let
+        mkAbbr = cmds:
+          cmds
+          |> lib.mapAttrs' (
+            n: v:
+              lib.nameValuePair "g${n}" {
+                position = "command";
+                cursor = true;
+                expansion = "git ${v}";
+              }
+          );
+      in
+        mkAbbr {
+          "" = "%";
+          d = "diff";
+          s = "status --short";
+          l = "log --oneline";
+          a = "add";
+          c = "commit";
+          ca = "commit --amend";
+          cm = ''commit -m \"%\"'';
+          p = "push";
+          u = "pull";
+          f = "fetch";
+          r = "reset";
+          ru = "reset --staged";
+          rh = "reset --hard";
+          rs = "reset --soft";
+          r1 = "reset HEAD~1";
+          co = "checkout";
+          cb = "checkout -b";
+          m = "merge";
+          i = "init";
+          cl = "clone";
+          st = "stash";
+          sta = "stash apply";
+          stp = "stash push";
+          stpm = ''stash push -m \"%\"'';
+          sto = "stash pop";
+          std = "stash drop";
+          sts = "stash show -p";
+        };
+
+      dotAbbreviations =
+        lib.range 1 5
+        |> map (n:
+          lib.nameValuePair
+          (lib.replicate (n + 1) "." |> lib.concatStrings)
+          (lib.replicate n ".." |> lib.concatStringsSep "/"))
+        |> lib.listToAttrs;
+
+      ltAbbreviations =
+        lib.range 1 9
+        |> map (n:
+          lib.nameValuePair "lt${toString n}" {
+            expansion = "lt -L ${toString n}";
+            position = "command";
+          })
+        |> lib.listToAttrs;
+
+      mkCmd = cmds:
+        cmds
+        |> lib.mapAttrs (_: v: {
+          position = "command";
+          expansion = v;
+        });
     in
-      {
-        c = {
-          expansion = "clear";
-          position = "command";
-        };
-        e = {
-          expansion = "yazi";
-          position = "command";
-        };
+      mkCmd {
+        c = "clear";
+        e = "yazi";
+        cp = "cp -vi";
+        mv = "mv -vi";
       }
+      // ltAbbreviations
       // nixAbbreviations
-      // gitAbbreviations;
+      // gitAbbreviations
+      // dotAbbreviations;
   };
 }
