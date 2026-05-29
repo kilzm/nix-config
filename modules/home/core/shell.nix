e40fd79cabb3b969201f9605cac3cc7d9e2633fe
Kilian Markl <kilian02.markl@gmail.com>
2026-05-29 16:55:49 +0200

    shell: update env var

--- modules/home/core/shell.nix
+++ modules/home/core/shell.nix
@@ -1,11 +1,11 @@
 {
-  flake.homeModules.shell = {self', ...}: {
+  flake.homeModules.shell = {self', lib, ...}: {
     programs.fish = {
       enable = true;
       package = self'.packages.fish;
     };
     home.sessionVariables = {
-      SHELL = "fish";
+      SHELL = lib.getExe self'.packages.fish;
     };
     programs.zoxide.enable = true;
   };
