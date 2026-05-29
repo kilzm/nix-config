410469f8adbeb0683754421334d6c370a0c79965
Kilian Markl <kilian02.markl@gmail.com>
2026-05-29 16:56:50 +0200

    gamescope: disable cap_sys_nice

--- modules/nixos/desktop/gaming/gaming.nix
+++ modules/nixos/desktop/gaming/gaming.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.gaming = {pkgs, ...}: {
     programs.gamescope = {
       enable = true;
-      capSysNice = true;
+      capSysNice = false;
       args = [
         "--rt"
         "--expose-wayland"
