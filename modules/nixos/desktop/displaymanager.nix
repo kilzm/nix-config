d5dd16cdc29b04319b92403edd4b1504beac0338
Kilian Markl <kilian02.markl@gmail.com>
2026-06-25 15:29:34 +0200

    displaymanager: greetd -> ly

--- modules/nixos/desktop/displaymanager.nix
+++ modules/nixos/desktop/displaymanager.nix
@@ -1,17 +1,7 @@
 {
   flake.nixosModules.displaymanager = {
-    lib,
-    pkgs,
-    ...
-  }: {
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${lib.getExe pkgs.tuigreet} --time --remember --remember-session --sessions ${pkgs.hyprland}/share/wayland-sessions";
-          user = "greeter";
-        };
-      };
+    services.displayManager = {
+      ly.enable = true;
     };
   };
 }
