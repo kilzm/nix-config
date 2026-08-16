3dee3c407d2f220b1208b0ea2768746f186356ad
Kilian Markl <kilian02.markl@gmail.com>
2026-08-16 15:22:20 +0200

    eris: remove nvidia

--- modules/hosts/eris/configuration.nix
+++ modules/hosts/eris/configuration.nix
@@ -12,12 +12,18 @@
     boot = {
       # kernelPackages = pkgs.linuxPackages_latest;
       kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-zen4;
-      kernelParams = ["amd_pstate=active" "mt7921e.disable_aspm=Y"];
-      kernelModules = ["i2c-dev" "i2cpiix4"];
+      kernelParams = [
+        "amd_pstate=active"
+        "mt7921e.disable_aspm=Y"
+        "video=DP-4:2560x1400@165"
+      ];
+      kernelModules = ["i2c-dev" "i2c-piix4"];
       supportedFilesystems.ntfs = true;
     };
 
-    hardware.amdgpu.initrd.enable = true;
+    hardware.amdgpu.overdrive.enable = true;
+
+    services.lact.enable = true;
 
     networking.hostName = "eris";
 
